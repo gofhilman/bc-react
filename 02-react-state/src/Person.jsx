df6a2edb855c6a0bbf028a9788f44b37d6ef6895
Hilman Fikry <hilman12fikry@gmail.com>
2025-11-28 18:45:27 +0700

    Add more react state example

---
+++ 02-react-state/src/Person.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function Person() {
+  const [person, setPerson] = useState({ name: "John", age: 100 });
+
+  // BAD - Don't do this!
+  const handleIncreaseAge = () => {
+    // mutating the current state object
+    person.age = person.age + 1;
+    setPerson(person);
+  };
+
+  // // GOOD - Do this!
+  // const handleIncreaseAge = () => {
+  //   console.log("in handleIncreaseAge (before setPerson call): ", person);
+  //   // copy the existing person object into a new object
+  //   // while updating the age property
+  //   const newPerson = { ...person, age: person.age + 1 };
+  //   setPerson(newPerson);
+  //   // we've called setPerson, surely person has updated?
+  //   console.log("in handleIncreaseAge (after setPerson call): ", person);
+  // };
+
+  // // this console.log runs every time the component renders
+  // // what do you think this will print?
+  // console.log("during render: ", person);
+
+  return (
+    <>
+      <h1>{person.name}</h1>
+      <h2>{person.age}</h2>
+      <button onClick={handleIncreaseAge}>Increase age</button>
+    </>
+  );
+}
+
+export default Person
