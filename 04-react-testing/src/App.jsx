b765ea24c7a9d879f0dce0818449a53ab37c1a3e
Hilman Fikry <hilman12fikry@gmail.com>
2025-12-01 10:02:49 +0700

    Add react testing example

---
+++ 04-react-testing/src/App.jsx
@@ -0,0 +1,24 @@
+// const App = () => <h1>Our First Test</h1>;
+
+// export default App;
+
+import { useState } from "react";
+
+const App = () => {
+  const [heading, setHeading] = useState("Magnificent Monkeys");
+
+  const clickHandler = () => {
+    setHeading("Radical Rhinos");
+  };
+
+  return (
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  );
+};
+
+export default App;
