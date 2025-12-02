79fb07c9bea08c56518b046467e53c56beffb4fe
Hilman Fikry <hilman12fikry@gmail.com>
2025-12-02 14:30:51 +0700

    Fix all examples

--- 01-my-first-react-app/src/main.jsx
+++ 01-my-first-react-app/src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 // import Greeting from './01-creating-components.jsx'
+// import Render from './03-rendering-techniques/01.jsx'
+// import RenderComponents from './03-rendering-techniques/02.jsx'
+// import RenderConditionally from './03-rendering-techniques/03.jsx'
+// import RenderConditionals from './03-rendering-techniques/04.jsx'
+// import TodoList from './04-keys.jsx'
+// import NoProp from './05-passing-data/01.jsx'
+// import Prop from './05-passing-data/02.jsx'
+// import PropFunction from './05-passing-data/03.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
